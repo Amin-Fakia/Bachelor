d22cea5d8126b31cf02b90b5e597310e4f118440
Amin Fakia <ameenofa@gmail.com>
2021-10-16 02:23:45 +0200

    more functions and stability

--- Project_Bac/functions.py
+++ Project_Bac/functions.py
@@ -3,19 +3,66 @@ import mne
 import scipy.interpolate as si
 from vedo import *
 from scipy.interpolate import Rbf
-from dsi_24_montage import ch_pos
-from vedo import settings, Line, show, interactive
-import tkinter as tk
+from vedo import show, interactive
 import matplotlib.pyplot as plt
 import time
+from PyQt5 import QtWidgets
+from matplotlib.widgets import SpanSelector
 
+mne.set_log_level(0)
 def get_data_from_raw_edf(s):
     if isinstance(s,str):
         raw = mne.io.read_raw_edf(s) 
         return raw.get_data()
+
+def get_times(s):
+    raw = mne.io.read_raw_edf(s)
+    df = raw.to_data_frame()
+    return df.iloc[:,0]
+def clean_ax(ax):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.get_yaxis().set_ticks([])
+
+def plot_data_from_edf(s):
+    fig,ax = plt.subplots()
+    times = [t/1000 for t in get_times(s)]
+    for d in get_data_from_raw_edf(s):
+        ax.plot(times,d, c='blue',linewidth=0.5)
+    plt.show()
+    
+def get_text(t1,t2):
+    return Text2D(f'{t1} - {t2} in s')   
+def animate_data_span(s,mesh,pts,data):
+    fig,ax = plt.subplots()
+    times = [t/1000 for t in get_times(s)]
+    for d in get_data_from_raw_edf(s):
+        line, = ax.plot(times,d, c='blue',linewidth=0.5)
+        
+    def onselect(xmin, xmax):
+        indmin, indmax = np.searchsorted(times, (xmin, xmax))
+        indmax = min(len(times) - 1, indmax)
+
+        region_x = times[indmin:indmax]
+        plt.close()
+        animate(mesh,pts,data,times.index(min(region_x)),times.index(max(region_x)),0.02,get_text(min(region_x),max(region_x)))
+
+
+    span = SpanSelector(ax, onselect, 'horizontal', useblit=True,
+                    rectprops=dict(alpha=0.5, facecolor='tab:red'))
+    try:
+        win = fig.canvas.manager.window
+    except AttributeError:
+        win = fig.canvas.window()
+    toolbar = win.findChild(QtWidgets.QToolBar)
+    toolbar.setVisible(False)
+    clean_ax(ax)
+    plt.show()
 def get_mesh(s):
     if isinstance(s,str):
-        mesh = Mesh("Head.obj")
+        mesh = Mesh(s)
         mesh.triangulate().clean().normalize()
         mesh.rotateX(90)
         mesh.rotateZ(180)
@@ -50,7 +97,10 @@ def RBF_Interpolation(mesh,pts,data):
     itr = Rbf(x, y, z, data) 
     xi, yi, zi = np.split(mesh.points(), 3, axis=1) 
     return itr(xi,yi,zi)
-def animate(mesh,pts,data,plot,t1,t2,f=0.02):
+
+    
+def animate(mesh,pts,data,t1,t2,f=0.02,text=Text2D('')):
+    plot = show(interactive=False)
     if t1 < 0:
         print("please insert a valid starting time-point")
     if t2 > len(data[0]):
@@ -60,7 +110,10 @@ def animate(mesh,pts,data,plot,t1,t2,f=0.02):
     for i in range(t1,t2):
         intpr = RBF_Interpolation(mesh,pts,[j[i] for j in data])
         mesh.cmap('jet', intpr,vmax=vmax,vmin=vmin)
-        plot.show(mesh)
+        plot.show(mesh,text)
+        plot.remove(text)
         time.sleep(f)
+    plot.close()
+    quit()
         
     
--- Project_Bac/main.py
+++ Project_Bac/main.py
@@ -2,14 +2,17 @@
 from functions import *
 from dsi_24_montage import ch_pos
 # make sure to support vedo: https://github.com/marcomusy/vedo
-
-data_set = get_data_from_raw_edf("Test_Data.edf")
-test_data = np.array([i[800] for i in data_set])
-mesh = get_mesh('Head.obj')
+s_edf = "..\\Bachelor\\Project_Bac\\Test_Data.edf"
+s_obj = "..\\Bachelor\\Project_Bac\\Head.obj"
+data_set = get_data_from_raw_edf(s_edf)
+# test_data = np.array([i[800] for i in data_set])
+mesh = get_mesh(s_obj)
 sensor_pts = get_sensor_3DLocations(ch_pos)
 pts = findVert(sensor_pts,mesh)
-li = Linear_Interpolation(mesh,sensor_pts,test_data)
-rb = RBF_Interpolation(mesh,pts,test_data)
-mesh.addQuality().cmap('jet', rb)
-plot = show(mesh,interactive=False)
-animate(mesh,pts,data_set,plot,t1=500,t2=700,f=0.01)
+# li = Linear_Interpolation(mesh,sensor_pts,test_data)
+# rb = RBF_Interpolation(mesh,pts,test_data)
+# mesh.addQuality().cmap('jet', rb)
+
+#plot_data_from_edf('Test_Data.edf')
+animate_data_span(s_edf,mesh,pts,data_set) # This is experimental still, run it on your own risk
+#animate(mesh,pts,data_set,t1=0,t2=7000,f=0.01)
