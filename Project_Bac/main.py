738471dc2e266861082f36892e5db46ff415d972
Amin Fakia <ameenofa@gmail.com>
2021-10-16 22:07:33 +0200

    test data

--- Project_Bac/main.py
+++ Project_Bac/main.py
@@ -12,14 +12,15 @@ raw = mne.io.read_raw_edf(s_edf)
 mesh = get_mesh(s_obj)
 sensor_pts = get_sensor_3DLocations(ch_pos)
 pts = findVert(sensor_pts,mesh)
+data = get_data_from_raw_edf(raw)
+test_data = [i[588] for i in data]
+li = Linear_Interpolation(mesh,sensor_pts,test_data)
+rb = RBF_Interpolation(mesh,pts,test_data)
+mesh.addQuality().cmap('jet', li)
 
-#li = Linear_Interpolation(mesh,sensor_pts,test_data)
-#rb = RBF_Interpolation(mesh,pts,test_data)
-#mesh.addQuality().cmap('jet', rb)
-
-#show(mesh)
+show(mesh)
 
 #plot_data_from_edf(raw)
 
-animate_data_span(raw,mesh,pts) # This is experimental still
+#animate_data_span(raw,mesh,pts) # This is experimental still
 #animate(mesh,pts,raw,t1=4000,t2=7000,f=0.01)
