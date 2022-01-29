3eeae81d594a0b25d66ed74372ea0fd40ab49e76
M1no <66644985+Amin-Fakia@users.noreply.github.com>
2022-01-29 12:21:23 +0100

    enhancements

--- Project_Bac/functions.py
+++ Project_Bac/functions.py
@@ -2,16 +2,34 @@ import numpy as np
 import mne
 import scipy.interpolate as si
 from vedo import *
-from scipy.interpolate import Rbf
+from scipy.interpolate import Rbf,RBFInterpolator
+
 from vedo import show, interactive
 import matplotlib.pyplot as plt
 import time
 from PyQt5 import QtWidgets
 from matplotlib.widgets import SpanSelector
+import keyboard
+import matplotlib.animation as animation
+from matplotlib.backend_bases import MouseButton
+from Cursor import Cursor
 mne.set_log_level(0)
 
+
+
+
 def get_data_from_raw_edf(raw):
-    return raw.get_data()
+    data = raw.get_data()[0:len(raw.get_data())-1]
+    f_data = []
+    
+    for i in range(0,len(data)):
+        if i == 13:
+            f_data.append(data[13])
+        elif i == 14:
+            f_data.append(data[14])
+        else:
+            f_data.append([0]*len(data[i]))
+    return f_data 
 
 def get_times(raw):
     df = raw.to_data_frame()
@@ -22,22 +40,41 @@ def clean_ax(ax):
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.get_yaxis().set_ticks([])
+def plot_data_from(raw,channels):
+    fig,ax = plt.subplots(len(raw))
+    c = 0
+    for d in raw:
 
-def plot_data_from_edf(raw):
-    fig,ax = plt.subplots()
-    times = [t/1000 for t in get_times(raw)]
-    for d in get_data_from_raw_edf(raw):
-        ax.plot(times,d, c='blue',linewidth=0.5)
+        ax[c].plot(range(len(d)),d, c='blue',linewidth=0.5)
+        ax[c].set_ylabel(f"{channels[c]}", rotation=0)
+       
+        clean_ax(ax[c])
+        c+=1
     plt.show()
-    
+def plot_data_from_edf(raw,channels):
+    fig,ax = plt.subplots(len(raw.get_data()))
+    times = [t/1000 for t in get_times(raw)]
+    c = 0
+    for d in get_data_from_raw_edf(raw):
+        ax[c].plot(times,d, c='blue',linewidth=0.5)
+        ax[c].set_ylabel(f"{channels[c]}", rotation=0)
+       
+        clean_ax(ax[c])
+        c+=1
+    plt.show()
+def plot_data(data):
+    fig,ax = plt.subplots()
+    ax.plot(range(0,len(data)), data)
+    plt.show()
 def get_text(t1,t2):
     return Text2D(f'{t1/1000} - {t2/1000} in s',s=2,c='r')   
-
+def get_average(data):
+    return np.average(data,axis=0)
 def animate_data_span(raw,mesh,pts):
     fig,ax = plt.subplots()
     times = [t/1000 for t in get_times(raw)]
     for d in get_data_from_raw_edf(raw):
-        line, = ax.plot(times,d, c='blue',linewidth=0.5)
+        line, = ax.plot(times,d,linewidth=0.5,c='blue')
         
     def onselect(xmin, xmax):
         indmin, indmax = np.searchsorted(times, (xmin, xmax))
@@ -45,7 +82,7 @@ def animate_data_span(raw,mesh,pts):
         
         region_x = times[indmin:indmax]
         plt.close() 
-        animate(mesh,pts,raw,times.index(min(region_x)),times.index(max(region_x)),0.02)
+        animate(mesh,pts,raw,times.index(min(region_x)),times.index(max(region_x)),0.01)
 
 
     span = SpanSelector(ax, onselect, 'horizontal', useblit=True,
@@ -64,13 +101,14 @@ def get_mesh(s):
         mesh.triangulate().clean().normalize()
         mesh.rotateX(90)
         mesh.rotateZ(180)
-        mesh.origin(0,-0.01,-0.04)
-        mesh.scale(0.09)
+        mesh.origin(0,-0.01,-0.05) # mesh.origin(0,-0.015,-0.05)
+        mesh.scale(0.09)#  mesh.scale(0.09)
         return mesh
 def get_sensor_3DLocations(l):
     pts = []
     for i, k in l.items():
-        pts.append([k[0],k[1],k[2]])
+        if i != 'TRG':
+            pts.append([k[0],k[1],k[2]])
     return pts
 def findMinD(x,pts,mesh):
     dist = []
@@ -92,36 +130,82 @@ def Linear_Interpolation(mesh,pts,data):
     
 def RBF_Interpolation(mesh,pts,data):
     x, y, z = np.split(np.array(pts), 3, axis=1)
-    itr = Rbf(x, y, z, data) 
+    itr = Rbf(x,y,z,data,function='gaussian')
     xi, yi, zi = np.split(mesh.points(), 3, axis=1) 
     return itr(xi,yi,zi)
 
 def plot_edf(raw):
-    raw.plot()
-    print([m for m in mne.read_annotations(raw)])
+    raw.plot(duration=30)
     plt.show()
-        
-def animate(mesh,pts,raw,t1,t2,f=0.02,text=''):
+def plot_tfa(raw):
+    raw.plot_psd()
+    plt.show()
+   
+def animate(mesh,pts,raw,t1,t2,f=1,text=''):
     # Rbf or Linear
+
     data = get_data_from_raw_edf(raw)
     times = get_times(raw)
+    
+    #print(len(times))
     if t1 < 0 and t1 < t2:
         print("please insert a valid starting time-point")
     if t2 > len(data[0]) and t2 > t1 :
         print("please insert a valid ending time-point")
     vmin = min([min(i[t1:t2]) for i in data])
+    #[t1:t2]
     vmax = max([max(i[t1:t2]) for i in data])
+    print("yo")
     text = get_text(times[t1],times[t2])
-    points= Points(pts,r=5,alpha=0.7,c='w')
+    
+    points= Points(pts,r=9,alpha=0.7,c='w')
     plot = show(interactive=False,bg='k')
+    datas= []
+    
     for i in range(t1,t2):
-
+        text2 = Text2D(f'\n \n {times[i]/1000} ')
         intpr = RBF_Interpolation(mesh,pts,[j[i] for j in data])
-        mesh.cmap('jet', intpr,vmax=vmax,vmin=vmin)
-        plot.show(mesh,points,text)
+        datas = [l[i] for l in data]
+        points.cmap('jet', datas, vmin=vmin, vmax=vmax)
+        mesh.cmap('jet', intpr, vmin=vmin, vmax=vmax)
+        plot.show(mesh,points,text,text2) # ,text2
         plot.remove(text)
+        plot.remove(text2)
         time.sleep(f)
     plot.close()
     quit()
+
+def enhanced_animation(raw,mesh,pts):
+    
+    fig,ax = plt.subplots()
+    times = [t/1000 for t in get_times(raw)]
+    data = get_data_from_raw_edf(raw)
+
+    try:
+        win = fig.canvas.manager.window
+    except AttributeError:
+        win = fig.canvas.window()
+    toolbar = win.findChild(QtWidgets.QToolBar)
+    toolbar.setVisible(False)
+
+
+    
+    for d in get_data_from_raw_edf(raw):
+        line, = ax.plot(times,d,linewidth=0.5,c='blue')
+    cursor = Cursor(ax)
+    fig.canvas.mpl_connect('motion_notify_event', cursor.on_mouse_move)
+    cid = fig.canvas.mpl_connect('button_press_event', cursor.on_mouse_click)
+    print(cid)
+    plot = show(interactive=False,bg='k')
+    i = 0
+    # while i < 9000:
+    #     text2 = Text2D(f'\n \n {times[i]/1000} ')
+    #     intpr = RBF_Interpolation(mesh,pts,[j[i] for j in data])
+    #     datas = [l[i] for l in data]
+    #     mesh.cmap('jet', intpr)
+    #     plot.show(mesh,text2) # ,text2
+    #     plot.remove(text2)
+    
+    plt.show()
         
     
--- Project_Bac/main.py
+++ Project_Bac/main.py
@@ -2,25 +2,80 @@
 from functions import *
 from dsi_24_montage import ch_pos
 import mne
+from mne.time_frequency import psd_welch
+from fooof import FOOOFGroup
+from fooof.bands import Bands
+from fooof.analysis import get_band_peak_fg
+from fooof.plts.spectra import plot_spectrum
+from mne.viz import plot_topomap
 # make sure to support vedo: https://github.com/marcomusy/vedo
+import numpy as np
+
+raw = mne.io.read_raw_edf("Data_01_filtered_01_30.edf",preload=True).drop_channels(['EEG CM-Pz','EEG X1-Pz','EEG X2-Pz','EEG X3-Pz'])
+#raw.filter(8,12,fir_design="firwin2")
+print(raw.info["ch_names"])
+
+raw.plot(duration=100)
+raw.plot_psd()
+s_edf = "Test_Data.edf"
+s_obj = "Head.obj"
+
+#raw.plot()
+
+data = get_data_from_raw_edf(raw)
+
+f_data = []
+for i in range(0,len(data)):
+    if i == 13:
+        f_data.append(data[13])
+    elif i == 14:
+        f_data.append(data[14])
+    else:
+        f_data.append([0]*len(data[i]))
+        
+print(len(data))
+    
+        
+        
 
 
-s_edf = "..\\Bachelor\\Project_Bac\\Test_Data.edf"
-s_obj = "..\\Bachelor\\Project_Bac\\Head.obj"
-raw = mne.io.read_raw_edf(s_edf)
+test_data = [1.4163747478831715e-06, 4.14125764170079e-06, 5.085762826688711e-06, 4.234749406127594e-06, 3.971483195731643e-06, 3.0456608715792426e-06, 1.2877115723770402e-06, 2.6315636904426714e-06, 4.833254160529337e-06, 6.262575272639049e-06, 8.030891584852211e-06, 6.973788155416181e-06, 3.0208964392932297e-06, 1.4670727629087545e-06, 1.6568362918653862e-06, 1.0343461100708259e-05, 1.4458459306627834e-05, 6.143860453501565e-06, 3.109986656619854e-06, 6.5331944370251785e-06]
 
+
+print(len(data))
 mesh = get_mesh(s_obj)
 sensor_pts = get_sensor_3DLocations(ch_pos)
+print(sensor_pts)
+print([d[0] for d in data])
 pts = findVert(sensor_pts,mesh)
-data = get_data_from_raw_edf(raw)
-test_data = [i[588] for i in data]
-li = Linear_Interpolation(mesh,sensor_pts,test_data)
+
+
+
 rb = RBF_Interpolation(mesh,pts,test_data)
-mesh.addQuality().cmap('jet', li)
+
+
+points = Points(pts)
+# for j in range(50):
+    
+#     test_data = [i[6000+j] for i in data]
+rb = RBF_Interpolation(mesh,pts,test_data)
+mesh.addQuality().cmap('jet', input_array=rb,arrayName="Quality", on="points")
+#     mesh.rotateX(-90)
+#     mesh.write(f"C:\\Users\\ameen\\OneDrive\\Desktop\\EEG_Data\\EEG_{j}.ply")
+#     mesh = get_mesh(s_obj)
+# print(len(data[0]))
+
+li = Linear_Interpolation(mesh,pts,test_data)
 
 
 
-#plot_data_from_edf(raw)
-
-animate_data_span(raw,mesh,pts) # This is experimental still
+#print(mesh.polydata())
+#mesh.rotateX(-90)
+#mesh.write("C:\\Users\\ameen\\OneDrive\\Desktop\\EEG_Data\\file.ply")
+#print(len(data))
+show(mesh,points)
+#enhanced_animation(raw,mesh,pts)
+animate_data_span(raw,mesh,pts)
+#plot_edf(raw)
+#animate(mesh,pts,raw,6000,6100) # This is experimental still
 #animate(mesh,pts,raw,t1=4000,t2=7000,f=0.01)
