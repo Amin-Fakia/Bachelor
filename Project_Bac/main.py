a674c7cb5a1a19345016340e5b7a099f37e69fb8
Amin Fakia <ameenofa@gmail.com>
2022-03-31 15:53:57 +0200

    Code Cleaning

--- Project_Bac/main.py
+++ Project_Bac/main.py
@@ -1,81 +1,255 @@
-# Imports
+# import the libraries
+
+import matplotlib.pyplot as plt
+import mne
+import numpy as np
+from matplotlib.widgets import Button
+from matplotlib.animation import FuncAnimation
+from vedo import show, interactive
 from functions import *
 from dsi_24_montage import ch_pos
-import mne
-from mne.time_frequency import psd_welch
-from fooof import FOOOFGroup
-from fooof.bands import Bands
-from fooof.analysis import get_band_peak_fg
-from fooof.plts.spectra import plot_spectrum
-from mne.viz import plot_topomap
-# make sure to support vedo: https://github.com/marcomusy/vedo
-import numpy as np
+import scipy
+from scipy.signal import savgol_filter
+import easygui
 
-raw = mne.io.read_raw_edf("Data_01_filtered_01_30.edf",preload=True).drop_channels(['EEG CM-Pz','EEG X1-Pz','EEG X2-Pz','EEG X3-Pz'])
-#raw.filter(8,12,fir_design="firwin2")
-print(raw.info["ch_names"])
+edf_file= easygui.fileopenbox()
+# Data_02_raw.edf
 
+head = "./3dmodel/Head.obj"
+raw = mne.io.read_raw_edf(edf_file ,preload=True).drop_channels(['EEG CM-Pz','EEG X1-Pz','EEG X2-Pz','EEG X3-Pz'])
+raw.filter(8,12)
 raw.plot(duration=100)
-raw.plot_psd()
-s_edf = "Test_Data.edf"
-s_obj = "Head.obj"
+fig, ax = plt.subplots(2)
+def get_times(raw):
+    df = raw.to_data_frame()
+    return df.iloc[:,0]
 
-#raw.plot()
-
-data = get_data_from_raw_edf(raw)
-
-f_data = []
-for i in range(0,len(data)):
-    if i == 13:
-        f_data.append(data[13])
-    elif i == 14:
-        f_data.append(data[14])
-    else:
-        f_data.append([0]*len(data[i]))
-        
-print(len(data))
+def plot_window(data,sampling_rate,win_size,step):
+    times = [i*(1/sampling_rate) for i in range(len(data))]
+    ft = np.abs(np.fft.rfft(data))
     
+    ps = np.square(ft)
+    frequency = np.linspace(0, sampling_rate/2, len(ps))
+    aspan_list = []
+    min_win = 0
+    max_win = win_size
+    ln, = ax[1].plot(frequency, ps)
+    
+    def nxt(event):
+        nonlocal max_win
+        nonlocal min_win
+        ax[1].cla()
+        aspan_list.append(ax[0].axvspan(min_win,max_win, color='red',alpha=0.2))
         
+        pos_x1 = int((min_win)*sampling_rate)
+        pos_x2 = int((max_win)*sampling_rate)
         
+        ft = np.abs(np.fft.rfft(data[pos_x1:pos_x2]))
+        ps = np.square(ft)
+        frequency = np.linspace(0, sampling_rate/2, len(ps))
+        
+        ax[1].set_ylim((0,10e-6))
+        ax[1].plot(frequency, ps,color='red')
+        
+        min_win +=step
+        max_win +=step
+        
+        plt.draw()
+    def init():
+        ax[1].set_ylim((0,10e-7))
+        return ln,    
+    def update(frame):
+        ax[1].cla()
+        nonlocal max_win
+        nonlocal min_win
+        aspan_list.append(ax[0].axvspan(min_win,max_win, color='red',alpha=0.2))
+        
+        pos_x1 = int((min_win)*sampling_rate)
+        pos_x2 = int((max_win)*sampling_rate)
+        
+        ft = np.abs(np.fft.rfft(data[pos_x1:pos_x2]))
+        ps = np.square(ft)
+        frequency = np.linspace(0, sampling_rate/2, len(ps))
+        ax[1].set_ylim((0,7e-6))
+        ax[1].plot(frequency,ps)
+        plt.draw()
+        min_win +=step
+        max_win +=step
+        return ln,
+
+    def play(event):
+        
+        plt.show()
+    
+    ax[0].plot(times,data)
+    ax[0].legend(["EEG O1"])
+    axnext = plt.axes([0.88, 0.05, 0.1, 0.075])
+    bnext = Button(axnext, 'Next')
+    bnext.on_clicked(nxt)
+    
+    plt.show()
+
+    
+def get_power_values(data,sampling_rate,win_size,step,itr):
+    
+    min_win = 0
+    max_win = win_size
+    data_array = []
+    while(max_win < itr):
+        pos_x1 = int((min_win)*sampling_rate)
+        pos_x2 = int((max_win)*sampling_rate)
+        
+        sums = []
+        
+        for d in data:
+            ft = np.abs(np.fft.rfft(d[pos_x1:pos_x2]))
+            ps = np.square(ft)
+            
+            sums.append(sum(ps))
+        
+        data_array.append(sums)
+        min_win +=step
+        max_win +=step
+    return np.transpose(data_array)
+        
+def get_ERP_values(data,sampling_rate,win_size,step,itr):
+    min_win = 0
+    max_win = win_size
+    data_array = []
+    while(max_win < itr):
+        pos_x1 = int((min_win)*sampling_rate)
+        pos_x2 = int((max_win)*sampling_rate)
+        sums = []
+        for d in data:
+            sums.append(sum(d[pos_x1:pos_x2]))
+        data_array.append(sums)
+        min_win +=step
+        max_win +=step
+    return np.transpose(data_array)      
+    
+    
 
 
-test_data = [1.4163747478831715e-06, 4.14125764170079e-06, 5.085762826688711e-06, 4.234749406127594e-06, 3.971483195731643e-06, 3.0456608715792426e-06, 1.2877115723770402e-06, 2.6315636904426714e-06, 4.833254160529337e-06, 6.262575272639049e-06, 8.030891584852211e-06, 6.973788155416181e-06, 3.0208964392932297e-06, 1.4670727629087545e-06, 1.6568362918653862e-06, 1.0343461100708259e-05, 1.4458459306627834e-05, 6.143860453501565e-06, 3.109986656619854e-06, 6.5331944370251785e-06]
+data = raw.get_data()
+erp_data = get_ERP_values(data,300,3,1,(len(data[0])/300))
 
 
-print(len(data))
-mesh = get_mesh(s_obj)
+
+
+O1 = data[13]
+plot_window(O1,300,3,1)
+
+
+dota = get_power_values(data,300,3,0.3,(len(data[0])/300))
+
+dota = dota.tolist()
+dota = [d for d in dota[:len(dota)-1]] # exclude TRG channel
+dota_smooth = []
+for d in dota:
+    dota_smooth.append(savgol_filter(d, 71, 3))
+plot_data(dota[13])    
+dota = dota_smooth
+
+from dsi_24_montage import chnls
+plot_data(dota[13])
+# with open("output.txt", "w") as txt_file:
+
+#     txt_file.write(str(dota)) # works with any number of elements in a line
+
+
+
+# dete = [[0]*len(dota[0])] * len(dota)
+# dete[13] = [d for d in dota[13]]
+# dete[14] = [d for d in dota[14]]
+
+
+
+# TODO: delete this
+
+
+#slider
+
+mesh = get_mesh(head)
+t1,t2 = 0,len(dota[0])
+vmin = min([min(i[t1:t2]) for i in dota])
+vmax = max([max(i[t1:t2]) for i in dota])
 sensor_pts = get_sensor_3DLocations(ch_pos)
-print(sensor_pts)
-print([d[0] for d in data])
-pts = findVert(sensor_pts,mesh)
-
-
-
-rb = RBF_Interpolation(mesh,pts,test_data)
-
-
-points = Points(pts)
-# for j in range(50):
+intpr = RBF_Interpolation(mesh,sensor_pts,[j[0] for j in dota])
+mesh.cmap('jet', intpr, vmin=-vmax, vmax=vmax).addScalarBar(pos=(0.8,0.3))
+def slider1(widget, event):
+    value = int(widget.GetRepresentation().GetValue())
     
-#     test_data = [i[6000+j] for i in data]
-rb = RBF_Interpolation(mesh,pts,test_data)
-mesh.addQuality().cmap('jet', input_array=rb,arrayName="Quality", on="points")
-#     mesh.rotateX(-90)
-#     mesh.write(f"C:\\Users\\ameen\\OneDrive\\Desktop\\EEG_Data\\EEG_{j}.ply")
-#     mesh = get_mesh(s_obj)
-# print(len(data[0]))
+    intpr = RBF_Interpolation(mesh,sensor_pts,[j[value] for j in dota])
+    mesh.cmap('jet', intpr, vmin=-vmax, vmax=vmax)
+def buttonfunc():
+    
+    if(bu.statusIdx == 0):
+        bu.switch()
+        txt = Text2D("Starting Animation",c='r')
+        
+        plot.show(txt,mesh,interactive=False)
+        for i in range(t1,t2):
+            if(bu.statusIdx ==0):
+                bu.switch()
+                break
+            txt2= Text2D(f"{i}",pos="top-right")
+            
+            intpr = RBF_Interpolation(mesh,sensor_pts,[j[i] for j in dota])
+            mesh.cmap('jet', intpr, vmin=-vmax, vmax=vmax)
+            plot.show(mesh,txt2)
+            time.sleep(1/20)
+            plot.remove(txt2)
+            
+        
+        plot.remove(txt)
 
-li = Linear_Interpolation(mesh,pts,test_data)
+    bu.switch()
+    plot.render()
+def save_btn():
+    
+    
+    for i in range(len(dota[0])):
+        rb = RBF_Interpolation(mesh,sensor_pts,[j[i] for j in dota])
+        mesh.addQuality().cmap('jet', input_array=rb,arrayName="Quality", on="points", vmin=-vmax, vmax=vmax)
+        mesh.write(f"ply_data\\EEG_{i}.ply")
+    sv_btn.switch()
+    
+    
+
+plot = Plotter(axes=0)
+sl = plot.addSlider2D(slider1, 0, len(dota[0])-1, value=0,
+               pos="bottom-right", title="Window Number",c='k')
+#scl = mesh.addScalarBar(pos=(0.8,0.15))
+bu = plot.addButton(
+    buttonfunc,
+    pos=(0.1, 0.05),  # x,y fraction from bottom left corner
+    states=["Play", "Stop"],
+    c=["w", "w"],
+    bc=["dg", "dv"],  # colors of states
+    font="courier",   # arial, courier, times
+    size=25,
+    bold=True,
+    italic=False,
+)
+sv_btn = plot.addButton(
+    save_btn,
+    pos=(0.2, 0.05),  # x,y fraction from bottom left corner
+    states=["Save","Saved"],
+    c=["w","w"],
+    bc=["dg", "dr"],  # colors of states
+    font="courier",   # arial, courier, times
+    size=25,
+    bold=True,
+    italic=False,
+)
+txt1 = Text2D(f"{((len(data[0])/300)/len(dota[0]))}",pos="bottom-left",c='w')
+plot.show(mesh,txt1,bg='w')
+plot.close()
 
 
 
-#print(mesh.polydata())
-#mesh.rotateX(-90)
-#mesh.write("C:\\Users\\ameen\\OneDrive\\Desktop\\EEG_Data\\file.ply")
-#print(len(data))
-show(mesh,points)
-#enhanced_animation(raw,mesh,pts)
-animate_data_span(raw,mesh,pts)
-#plot_edf(raw)
-#animate(mesh,pts,raw,6000,6100) # This is experimental still
-#animate(mesh,pts,raw,t1=4000,t2=7000,f=0.01)
+
+
+
+
+
